ec605a40118af0f922dc4fcec95b4d7be85953a9
SukhaIryna <suhayairina1@gmail.com>
2023-06-07 21:32:37 +0200

    my homework

--- src/route/index.js
+++ src/route/index.js
@@ -12,7 +12,13 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з container
-  res.render('index', {})
+  res.render('index', { 
+  layout: 'default',
+
+  page: {
+    title: 'Practice Page',
+  },
+})
   //                  ↑↑ сюди вводимо JSON дані
 })
 
